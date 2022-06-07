b58f2cffcb173a7a7c47ba97c5900fbaaa55d429
kishorejcodes <107037868+kishorejcodes@users.noreply.github.com>
2022-06-07 16:01:17 +0530

    Update and rename currentCheck.py to program_to_check_bulb_glows_or_not.py

--- program_to_check_bulb_glows_or_not.py
+++ program_to_check_bulb_glows_or_not.py
@@ -26,6 +26,7 @@ def oppositeConnection(way):
     elif (way == "up"): return "down"
     elif (way == "down"): return "up"
 
+    
 matrix = []
 row1 = [ ["right", "down"],["left", "right"],["right"],["left","down"],["up","right"] ]
 row2 = [ ["up", "right"],["left","right","down"],["left"],["up","down"],["up","down"] ]
@@ -34,8 +35,32 @@ row4 = [ ["left"],["up","right"],["left","up","down"],["right"],["left"] ]
 row5 = [ ["left","down"],["left","right"],["left","right","up"],["left","right"],["down"] ]
 matrix.extend([row1, row2, row3, row4, row5])
 
-src = [4, 1]
-dest = [0, 2]
+src = [4, 1]    # coordinates of Current Source
+dest = [0, 2]   # coordinates of Bulb to glow
 visited = set()
 
-print(hasPath(matrix, src, dest, visited))
+result = hasPath(matrix, src, dest, visited)
+print(result)
+
+
+
+'''
+Problem Statemtnt:
+Given some grid with hands of metals pointed to any of four directions(L,R,U,D) from center at each grid box.
+and also provided with the coordinates of current source and bulb .Now we have to check whether the bulb glows or not. To achieve this we just
+have to check whether path exists between given source and destination.
+
+TestCase:
+#provided Data of each grid box connections 
+Input : 4 1
+        0 2
+        
+Output : True
+
+
+TestCase:
+Input : 4 1
+        0 1
+Output : False
+
+'''
